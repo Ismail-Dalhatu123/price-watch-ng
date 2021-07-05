21cbc27d4ff85d676d2d0b888fc3edb0299bc106
Yunusa <ygarbawala@gmail.com>
2021-07-05 09:16:38 +0100

    Add

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import AppContext from "./contexts/AppContext";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import Login from "./components/auth/Login";
 import Home from "./components/admin/Home";
+import decodeJWT from "./utils/decodeJWT";
 
 function App(props) {
   const [theme, setTheme] = useState(localStorage.getItem("theme") || "light");
   const [user, setUser] = useState(null);
+
+  const restoreUser = () => {
+    const token = decodeJWT();
+    if (token.succes) setUser(token.tokenData);
+  };
+
+  useEffect(() => {
+    restoreUser();
+  });
 
   return (
     <AppContext.Provider value={{ theme, setTheme, user, setUser }}>
--- src/api/client.js
+++ src/api/client.js
@@ -3,7 +3,9 @@ import axios from "axios";
 const client = axios.create({
   baseURL: "https://price-watch-backend.herokuapp.com/",
   headers: {
-    "x-auth-token": localStorage.getItem("PWtoken"),
+    "x-auth-token":
+      localStorage.getItem("PWtoken") ||
+      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJGaXJzdE5hbWUiOiJJc21haWwiLCJMYXN0TmFtZSI6IkRhbGhhdHUiLCJFbWFpbCI6ImlzbWFpbGRhbGhhdHU0NDJAZ21haWwuY29tIiwiX2lkIjoiNjBkY2RiYjZmMTEwMzc1YTcwMDExMDk0IiwiaXNTdXBlciI6dHJ1ZSwiaXNBZG1pbiI6dHJ1ZSwiZXhwaXJ5IjpudWxsLCJpYXQiOjE2MjU0NzI5MDd9.l8i6PRy2JOhFRTB25sOe_dUW3wWGJkhB5YOoExJX5pk",
   },
 });
 
