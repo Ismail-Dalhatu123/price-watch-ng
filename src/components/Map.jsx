6a05253b9570e1e45257b90055c034b8bf87f728
Yunusa <ygarbawala@gmail.com>
2021-07-09 13:03:24 +0100

    Add

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,37 +1,66 @@
 import React, { Component } from 'react'
-
+import AppContext from '../contexts/AppContext';
 import { Map, InfoWindow, Marker, GoogleApiWrapper } from 'google-maps-react';
- 
-export class MapContainer extends Component {
-    state = {
-        selectedPlace: { name: 'name' }
-    }
-    onMarkerClick() {
-        alert('Click')
-    }
-    onInfoWindowClose() {
-        alert('Close')
-    }
-    
-    render() {
-      return (
-      <div style={{width: window.innerWidth - 300, height: window.innerHeight - 320, overflow: 'hidden', position: 'relative',marginLeft: 'auto', marginRight: 'auto'}}>
-        <Map style={{ width: window.innerWidth - 300, height: window.innerHeight - 320,borderRadius: 10, }} google={this.props.google} zoom={14}>
-  
-          {/* <Marker onClick={this.onMarkerClick}
-                  name={'Current location'} />
-  
-          <InfoWindow onClose={this.onInfoWindowClose}>
-              <div>
-                <h1>{this.state.selectedPlace.name}</h1>
-              </div>
-          </InfoWindow> */}
-        </Map>
-      </div>
-    );
-  }
+import mapStyles from '../utils/mapstyles';
+import { useContext } from 'react';
+
+function AppMap({ google }) {
+  const { theme } = useContext(AppContext)
+  return (
+    <div style={{width: window.innerWidth - 300, height: window.innerHeight - 320, overflow: 'hidden', position: 'relative',marginLeft: 'auto', marginRight: 'auto'}}>
+      <Map
+                
+        styles={theme === 'light' ? [] : mapStyles} style={{ width: window.innerWidth - 300, height: window.innerHeight - 320, borderRadius: 10, }} google={google} zoom={14}>
+        
+        {/* <Marker onClick={this.onMarkerClick}
+                name={'Current location'} />
+        
+        <InfoWindow onClose={this.onInfoWindowClose}>
+            <div>
+              <h1>{this.state.selectedPlace.name}</h1>
+            </div>
+        </InfoWindow> */}
+      </Map>
+    </div>
+  );
 }
+
+// export class MapContainer extends Component {
+//     state = {
+//         selectedPlace: { name: 'name' }
+//     }
+//     onMarkerClick() {
+//         alert('Click')
+//     }
+//     onInfoWindowClose() {
+//         alert('Close')
+//     }
+    
+//     render() {
+//       return (
+//         <AppContext.Consumer>
+//           {context => (
+//             <div style={{width: window.innerWidth - 300, height: window.innerHeight - 320, overflow: 'hidden', position: 'relative',marginLeft: 'auto', marginRight: 'auto'}}>
+//               <Map
+                
+//                 styles={context.theme === 'light' ? [] : mapStyles} style={{ width: window.innerWidth - 300, height: window.innerHeight - 320, borderRadius: 10, }} google={this.props.google} zoom={14}>
+        
+//                 {/* <Marker onClick={this.onMarkerClick}
+//                         name={'Current location'} />
+        
+//                 <InfoWindow onClose={this.onInfoWindowClose}>
+//                     <div>
+//                       <h1>{this.state.selectedPlace.name}</h1>
+//                     </div>
+//                 </InfoWindow> */}
+//               </Map>
+//             </div>
+//           )}
+//         </AppContext.Consumer>
+//     );
+//   }
+// }
  
 export default GoogleApiWrapper({
   apiKey: ('AIzaSyDhV68bTduvMWSKWJAQ9i8W3UfxLErYDGw')
-})(MapContainer)
+})(AppMap)
--- src/components/Maps.jsx
+++ src/components/Maps.jsx
@@ -1,8 +1,37 @@
 import React from 'react';
+import { useEffect } from 'react';
+import { useState } from 'react';
+import { useContext } from 'react';
+import { geocodeByAddress, getLatLng } from 'react-places-autocomplete';
+import { toast } from 'react-toastify';
+import AdminContext from '../contexts/AdminContext';
 import getDarkClass from '../utils/getDarkClass';
 import Map from './Map';
 
 function Maps(props) {
+    const { registeredMarkets } = useContext(AdminContext)
+    const [marketMarkers, setMarketMarkers] = useState([])
+
+    const loadMarkers = () => {
+        const list = []
+        for (let i = 0; i < registeredMarkets.length; i++) {
+            const mkt = registeredMarkets[i];
+            // const res = await geocodeByAddress(mkt.Location)
+            // const ltn = await getLatLng(res[0])
+            // console.log(ltn)
+            // geocodeByAddress(mkt.Location)
+            //     .then(result => getLatLng(result[0]))
+            //     .then(latlng => list.push(latlng))
+            //     .catch(err => console.log('An error occured', err))
+        
+        }
+        setMarketMarkers(list)
+        console.log(list)
+        console.log(registeredMarkets)
+    }
+    useEffect(() => {
+        loadMarkers()
+    },[])
     return (
         <div className={`maps sh flex flex-column justify-center align-center ${getDarkClass('dark-accent')}`}>
             <div className="desc sh"></div>
