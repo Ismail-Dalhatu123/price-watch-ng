85497a056fa8822668a4fd423d1189ed68e83de1
Ismail Dalhatu <ismaildalhatu442@gmail.com>
2022-02-03 20:45:16 +0100

    Log2

--- src/components/MapPopup.jsx
+++ src/components/MapPopup.jsx
@@ -11,7 +11,7 @@ const MapPopup = ({ coords, closeMap = () => {} }) => {
       <div className="map_container">
         <GoogleMap
           hideText={false}
-          markers={[{ lat: coords.latitude, lng: coords.longitude }]}
+          markers={[{ lat: coords.Latitude, lng: coords.Longitude }]}
           width={window.innerWidth * 0.8}
           height={window.innerHeight * 0.9}
         />
