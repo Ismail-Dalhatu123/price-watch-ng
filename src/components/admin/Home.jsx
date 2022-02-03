be5e037057f6e6b47276825cee4bfe9280e185b0
Ismail Dalhatu <ismaildalhatu442@gmail.com>
2022-02-03 21:11:29 +0100

    Remove Theme Toggler

--- src/components/admin/Home.jsx
+++ src/components/admin/Home.jsx
@@ -4,9 +4,9 @@ import React, { useEffect, useState } from "react";
 // import Dashboard from "./routes/Dashboard";
 // import Header from "../Header";
 // import getDarkClass from "../../utils/getDarkClass";
-import url from "../../api/urls";
-import { GET } from "../../api/methods";
-import { toast } from "react-toastify";
+// import url from "../../api/urls";
+// import { GET } from "../../api/methods";
+// import { toast } from "react-toastify";
 // import Loader from "../Loader";
 // import AdminContext from "../../contexts/AdminContext";
 // import Agents from "./routes/Agents";
@@ -32,135 +32,130 @@ function Home(props) {
   const [categories, setCategories] = useState([]);
   const [commoditiesList, setCommodities] = useState([]);
 
-  useEffect(() => {
-    // const token = decodeJWT();
-    // if(!token.succes) window.location = '/'
-  }, []);
-
-  const loadStates = async (q = false) => {
-    if (!q) {
-      setLoading(true);
-    }
-    const statesList = await GET(url.states);
-    if (statesList.ok) {
-      setRegistredStates(statesList.response.data);
-    } else {
-      toast.error("An error occured");
-    }
-    if (!q) {
-      setLoading(false);
-    }
-  };
-  const loadAgents = async (q = false) => {
-    if (!q) {
-      setLoading(true);
-    }
-    const agentsList = await GET(url.agents.base);
-    if (agentsList.ok) {
-      setAgents(agentsList.response.data);
-    } else {
-      toast.error("An error occured");
-    }
-    if (!q) {
-      setLoading(false);
-    }
-  };
-  const loadMarkets = async (q = false) => {
-    if (!q) {
-      setLoading(true);
-    }
-    const marketList = await GET(url.market);
-    if (marketList.ok) {
-      setMarkets(marketList.response.data);
-    } else {
-      toast.error("An error occured");
-    }
-    if (!q) {
-      setLoading(false);
-    }
-  };
-  const loadRegions = async (q = false) => {
-    if (!q) {
-      setLoading(true);
-    }
-    const regionList = await GET(url.region);
-    if (regionList.ok) {
-      setRegions(regionList.response.data);
-    } else {
-      toast.error("An error occured");
-    }
-    if (!q) {
-      setLoading(false);
-    }
-  };
-  const loadCommodities = async (q = false) => {
-    if (!q) {
-      setLoading(true);
-    }
-    const commoditiesList = await GET(url.commodities);
-    if (commoditiesList.ok) {
-      setCommodities(commoditiesList.response.data);
-    } else {
-      toast.error("An error occured");
-    }
-    if (!q) {
-      setLoading(false);
-    }
-  };
-  const loadLocalGovs = async (q = false) => {
-    if (!q) {
-      setLoading(true);
-    }
-    const lgList = await GET(url.localGov);
-    if (lgList.ok) {
-      setLocalGovs(lgList.response.data);
-    } else {
-      toast.error("An error occured");
-    }
-    if (!q) {
-      setLoading(false);
-    }
-  };
-  const loadQuantities = async (q = false) => {
-    if (!q) {
-      setLoading(true);
-    }
-    const QuantityList = await GET(url.quantities);
-    if (QuantityList.ok) {
-      setQuantities(QuantityList.response.data);
-    } else {
-      toast.error("An error occured");
-    }
-    if (!q) {
-      setLoading(false);
-    }
-  };
-  const loadCategories = async (q = false) => {
-    if (!q) {
-      setLoading(true);
-    }
-    const categoryList = await GET(url.categories);
-    if (categoryList.ok) {
-      setCategories(categoryList.response.data);
-    } else {
-      toast.error("An error occured");
-    }
-    if (!q) {
-      setLoading(false);
-    }
-  };
-  useEffect(() => {
-    setLoading(true);
-    loadStates(true);
-    loadLocalGovs(true);
-    loadAgents(true);
-    loadRegions(true);
-    loadMarkets(true);
-    loadCommodities(true);
-    loadQuantities(true);
-    loadCategories(true);
-    setLoading(false);
-  }, []);
+  // const loadStates = async (q = false) => {
+  //   if (!q) {
+  //     setLoading(true);
+  //   }
+  //   const statesList = await GET(url.states);
+  //   if (statesList.ok) {
+  //     setRegistredStates(statesList.response.data);
+  //   } else {
+  //     toast.error("An error occured");
+  //   }
+  //   if (!q) {
+  //     setLoading(false);
+  //   }
+  // };
+  // const loadAgents = async (q = false) => {
+  //   if (!q) {
+  //     setLoading(true);
+  //   }
+  //   const agentsList = await GET(url.agents.base);
+  //   if (agentsList.ok) {
+  //     setAgents(agentsList.response.data);
+  //   } else {
+  //     toast.error("An error occured");
+  //   }
+  //   if (!q) {
+  //     setLoading(false);
+  //   }
+  // };
+  // const loadMarkets = async (q = false) => {
+  //   if (!q) {
+  //     setLoading(true);
+  //   }
+  //   const marketList = await GET(url.market);
+  //   if (marketList.ok) {
+  //     setMarkets(marketList.response.data);
+  //   } else {
+  //     toast.error("An error occured");
+  //   }
+  //   if (!q) {
+  //     setLoading(false);
+  //   }
+  // };
+  // const loadRegions = async (q = false) => {
+  //   if (!q) {
+  //     setLoading(true);
+  //   }
+  //   const regionList = await GET(url.region);
+  //   if (regionList.ok) {
+  //     setRegions(regionList.response.data);
+  //   } else {
+  //     toast.error("An error occured");
+  //   }
+  //   if (!q) {
+  //     setLoading(false);
+  //   }
+  // };
+  // const loadCommodities = async (q = false) => {
+  //   if (!q) {
+  //     setLoading(true);
+  //   }
+  //   const commoditiesList = await GET(url.commodities);
+  //   if (commoditiesList.ok) {
+  //     setCommodities(commoditiesList.response.data);
+  //   } else {
+  //     toast.error("An error occured");
+  //   }
+  //   if (!q) {
+  //     setLoading(false);
+  //   }
+  // };
+  // const loadLocalGovs = async (q = false) => {
+  //   if (!q) {
+  //     setLoading(true);
+  //   }
+  //   const lgList = await GET(url.localGov);
+  //   if (lgList.ok) {
+  //     setLocalGovs(lgList.response.data);
+  //   } else {
+  //     toast.error("An error occured");
+  //   }
+  //   if (!q) {
+  //     setLoading(false);
+  //   }
+  // };
+  // const loadQuantities = async (q = false) => {
+  //   if (!q) {
+  //     setLoading(true);
+  //   }
+  //   const QuantityList = await GET(url.quantities);
+  //   if (QuantityList.ok) {
+  //     setQuantities(QuantityList.response.data);
+  //   } else {
+  //     toast.error("An error occured");
+  //   }
+  //   if (!q) {
+  //     setLoading(false);
+  //   }
+  // };
+  // const loadCategories = async (q = false) => {
+  //   if (!q) {
+  //     setLoading(true);
+  //   }
+  //   const categoryList = await GET(url.categories);
+  //   if (categoryList.ok) {
+  //     setCategories(categoryList.response.data);
+  //   } else {
+  //     toast.error("An error occured");
+  //   }
+  //   if (!q) {
+  //     setLoading(false);
+  //   }
+  // };
+  // useEffect(() => {
+  //   setLoading(true);
+  //   loadStates(true);
+  //   loadLocalGovs(true);
+  //   loadAgents(true);
+  //   loadRegions(true);
+  //   loadMarkets(true);
+  //   loadCommodities(true);
+  //   loadQuantities(true);
+  //   loadCategories(true);
+  //   setLoading(false);
+  // }, []);
   return <h2>Cool</h2>;
   // return (
   //   <AdminContext.Provider
