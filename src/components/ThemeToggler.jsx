644cd3ed0268e92698834fe4a87e90596b5a8b6e
Ismail Dalhatu <ismaildalhatu442@gmail.com>
2022-02-03 20:20:15 +0100

    Remove Theme Toggler

--- src/components/ThemeToggler.jsx
+++ src/components/ThemeToggler.jsx
@@ -1,12 +1,12 @@
-import React, { useContext } from 'react';
-import AppContext from '../contexts/AppContext';
-import WbSunnyRoundedIcon from '@material-ui/icons/WbSunnyRounded';
-import Brightness2RoundedIcon from '@material-ui/icons/Brightness2Rounded';
+import React, { useContext } from "react";
+import AppContext from "../contexts/AppContext";
+import WbSunnyRoundedIcon from "@material-ui/icons/WbSunnyRounded";
+import Brightness2RoundedIcon from "@material-ui/icons/Brightness2Rounded";
 
 function ThemeToggler({ className }) {
-    const { theme, setTheme } = useContext(AppContext)
-    
-    const changeTheme = () => {
+  const { theme, setTheme } = useContext(AppContext);
+  return null;
+  const changeTheme = () => {
     if (theme === "light") {
       localStorage.setItem("theme", "dark");
       setTheme("dark");
@@ -15,8 +15,17 @@ function ThemeToggler({ className }) {
     localStorage.setItem("theme", "light");
     setTheme("light");
   };
-    if(theme === 'light') return <div style={{cursor: 'pointer'}} className={className}><Brightness2RoundedIcon onClick={changeTheme} /></div>
-    return <div style={{color:'#fff', cursor: 'pointer'}} className={className}><WbSunnyRoundedIcon onClick={changeTheme} /></div>
+  if (theme === "light")
+    return (
+      <div style={{ cursor: "pointer" }} className={className}>
+        <Brightness2RoundedIcon onClick={changeTheme} />
+      </div>
+    );
+  return (
+    <div style={{ color: "#fff", cursor: "pointer" }} className={className}>
+      <WbSunnyRoundedIcon onClick={changeTheme} />
+    </div>
+  );
 }
 
 export default ThemeToggler;
